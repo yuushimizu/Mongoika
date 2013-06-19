b70425fee126c18f07395ff8794d7eb9ac014700
Yuu Shimizu <yuu@nyampass.com>
2013-06-19 12:07:17 +0900

    0.8.0 Release: Modify to use clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject mongoika "0.7.9"
+(defproject mongoika "0.8.0"
   :description "Clojure MongoDB Library"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.mongodb/mongo-java-driver "2.7.2"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]])
